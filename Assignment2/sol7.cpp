385e6bfd94cc835bbfd5730174ad3251f6b4bc56
ditya17 <96773479+ditya17@users.noreply.github.com>
2022-05-04 21:57:46 +0530

    Create sol7.cpp

---
+++ Assignment2/sol7.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n){
+     if (n<=1){
+        return false;
+    }
+    else
+       for (int i=2;i<n;i++){
+           if (n%i==0){
+               return false;
+           }
+       }
+    return true;
+              
+}
+int main() {
+    int p;
+    cin>>p;
+    if ( isprime(p)==true ){
+        cout<<"prime";
+    }
+    else {
+        cout<<"not prime";
+    }
+}
