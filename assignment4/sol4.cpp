f82664c1e5fdc21142fc17bad0c89cb144d69c0f
ditya17 <96773479+ditya17@users.noreply.github.com>
2022-05-09 23:10:32 +0530

    Create sol4.cpp

---
+++ assignment4/sol4.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int addDigits(int n) {
+        int sum =0;
+        while (n>0){
+         sum = sum+n%10;
+         n = n/10;
+       }
+        if(sum>9) return addDigits(sum);
+        return sum;
+   }
+};
