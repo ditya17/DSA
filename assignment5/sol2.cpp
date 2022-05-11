42671f5d0d94fdb605de2d54eb97b8dd449899ff
ditya17 <96773479+ditya17@users.noreply.github.com>
2022-05-11 10:31:18 +0530

    Create sol2.cpp

---
+++ assignment5/sol2.cpp
@@ -0,0 +1,22 @@
+ vector<int>getPrimeFactors(int a){
+        vector<int>Factors;
+         for(int i=2;i<=a;i++){
+          while(a%i==0){
+            Factors.push_back(i);
+            a=a/i;
+              }
+         }
+          return Factors;
+ }
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int>P = getPrimeFactors(n);
+        int sum =0;
+       for(int i=0;i<P.size();i++){
+       sum = sum+ P[i];  
+       } 
+return sum;
+        
+    }
+};
