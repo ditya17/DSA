3e35b9c882d0485efd10b4c9ae5011e440f825a7
ditya17 <96773479+ditya17@users.noreply.github.com>
2022-05-30 10:04:38 +0530

    Create subtraction_of_two_strings.cpp

---
+++ inclass2/subtraction_of_two_strings.cpp
@@ -0,0 +1,72 @@
+bool compare(string a,string b){
+    int i=0; 
+    int j=0;
+    while(i<a.size() && a[i]=='0') i++;
+    while(j<b.size() && b[j]=='0') j++;
+    
+    if ((a.size()-i)>(b.size()-j))    return 1;
+    else if ((a.size()-i)<(b.size()-j))    return 0;
+    else {
+        while(i<a.size()){
+            if(a[i]>b[j]) return 1;
+            if(a[i]<b[j]) return 0;
+            i++; j++;
+        }
+    }
+    return 1;
+}
+
+string subtract(string a,string b){
+    string d = "0123456789";
+    string c ="";
+    compare (a,b);
+    if(compare(a,b) == 1) {
+        int i = a.size()-1;
+        int j = b.size()-1;
+        while (i>=0 && j>=0){
+            if ((a[i]-'0')>=(b[j]-'0')) {
+               int z = (a[i]-'0')-(b[j]-'0');
+                c = c+ d[z];
+            }
+            else{
+                int z = ((a[i]-'0')+10)-(b[j]-'0');
+                a[i-1] = ((a[i-1]-'0')-1)+'0';
+                c = c+ d[z]; 
+            }
+          i--,j--;
+        }
+        while (i>=0){
+            c = c+a[i];
+            i--;
+         }
+    }
+    else  {
+        int i = a.size()-1;
+        int j = b.size()-1;
+        while (j>=0 && i>=0){
+            if ((b[j]-'0')>=(a[i]-'0')){
+               int z = (b[j]-'0')-(a[i]-'0');
+                c = c+ d[z];
+            }
+            else{
+                int z = ((b[j]-'0')+10)-(a[i]-'0');
+                b[j-1] = ((b[j-1]-'0')-1)+'0';
+                c = c+ d[z]; 
+            }
+          i--,j--;
+        }
+        while (j>=0){
+            c = c+b[j];
+            j--;
+         }
+    }
+    reverse(c.begin(),c.end());
+    cout<<c;
+    return c;
+}
+
+int main(){
+    string a,b;
+    cin>>a>>b;
+    subtract(a,b);
+}
