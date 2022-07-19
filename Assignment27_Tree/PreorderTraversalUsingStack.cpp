ffdf95b50666d3734badf1bdcb1851e5bbaac0a8
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-07-19 13:12:15 +0530

    Update PreorderTraversalUsingStack.cpp

--- Assignment27_Tree/PreorderTraversalUsingStack.cpp
+++ Assignment27_Tree/PreorderTraversalUsingStack.cpp
@@ -1,21 +1,47 @@
+// class Solution {
+// public:
+//     vector<int>A;
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         stack<TreeNode*>S;
+//         if (root == NULL) return A;
+//         S.push(root);
+//         while (!S.empty()){
+//             TreeNode * temp = S.top();
+//             A.push_back(temp->val);
+//             S.pop();
+//             if(temp->left){
+//                 preorderTraversal(temp->left);
+//             }
+//             if(temp->right){
+//                 preorderTraversal(temp->right);
+//             }
+//         }
+//     return A;
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<int>A;
     vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*>S;
+        vector<int>A;
+        stack<TreeNode*>s;
+        s.push(root);
+        TreeNode* temp;
         if (root == NULL) return A;
-        S.push(root);
-        while (!S.empty()){
-            TreeNode * temp = S.top();
-            A.push_back(temp->val);
-            S.pop();
-            if(temp->left){
-                preorderTraversal(temp->left);
+        while (!s.empty()){
+            temp = s.top();
+                A.push_back(temp->val);
+            s.pop();
+            if (temp->right){
+                s.push(temp->right);
+                temp->right = NULL;
             }
-            if(temp->right){
-                preorderTraversal(temp->right);
+            if (temp->left){
+                s.push(temp->left);
+                temp->left = NULL;
             }
         }
-    return A;
+        return A;
     }
 };
