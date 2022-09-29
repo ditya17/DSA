4878857f6b661c33f58ee314a021ce1cd948d981
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-09-30 03:02:30 +0530

    Update Lowest_Common_Multiple.cpp

--- Assignment2_Maths/Lowest_Common_Multiple.cpp
+++ Assignment2_Maths/Lowest_Common_Multiple.cpp
@@ -1,1 +1,16 @@
-.
+// TLE for long values
+
+long Solution::solve(int A, int B) {
+    if (B>A) swap(A,B);
+    if (A%B == 0) return A;
+    long product =1;
+    for (int i = 2;i<=B;i++){
+        while (A%i == 0 && B%i == 0){
+        product = product*i;
+            A = A/i;
+            B = B/i;
+            }
+        }
+    product = product*A*B;
+    return product;
+}
