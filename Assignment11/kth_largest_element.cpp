5636de8c466108ccffc7698e28411b3e46511146
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-10-09 00:57:44 +0530

    Update kth_largest_element.cpp

--- Assignment11/kth_largest_element.cpp
+++ Assignment11/kth_largest_element.cpp
@@ -1,12 +1,12 @@
 // without using binary search
 
-// class Solution {
-// public:
-//     int findKthLargest(vector<int>& nums, int k) {
-//         sort(nums.begin(),nums.end());
-//         return nums[nums.size()-k];
-//     }
-// };
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()-k];
+    }
+};
 
 // with binary search
 
