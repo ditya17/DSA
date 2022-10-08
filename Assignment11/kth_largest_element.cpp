431ff8813022e7baae23497b32f8b9d9da1a8fef
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-10-09 00:57:06 +0530

    Update kth_largest_element.cpp

--- Assignment11/kth_largest_element.cpp
+++ Assignment11/kth_largest_element.cpp
@@ -10,3 +10,31 @@
 
 // with binary search
 
+class Solution {
+public:
+     int smallerelements(vector<int>& nums, int k){
+        int count=0;
+        for (int i =0;i<nums.size();i++){
+            if (nums[i]<=k) count++;
+        }
+        return count;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        k = nums.size()-k+1;
+        if (nums.size()==1) return nums[0];
+        int low = -10000, high = 10000;
+        int ans = low;
+        while (low<=high){
+            int mid = low + (high - low)/2;
+            int x = smallerelements(nums,mid);
+            if (x<k) {
+                low = mid+1;
+            }
+            else if (x>=k){
+                ans = mid;
+                high = mid -1;
+            }
+        }
+        return ans;
+    }
+};
