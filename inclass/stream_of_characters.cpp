a3b630aa81ec71726855a6790ca3d4d54d2502aa
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-10-11 00:48:20 +0530

    Create stream_of_characters.cpp

---
+++ inclass/stream_of_characters.cpp
@@ -0,0 +1,6 @@
+// getline is used to take stream of characters because we cannot take a string as an input with spaces.
+int main(){
+   string my_str;
+   getline (cin, my_str);
+   cout<<my_str;
+}
