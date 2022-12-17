2bac99820954d132fc3f9a2cf02d15b5ad8cdcaf
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2022-12-17 11:46:51 +0530

    Update reverse_the_string.cpp

--- Assignment3_String/reverse_the_string.cpp
+++ Assignment3_String/reverse_the_string.cpp
@@ -1,23 +1,18 @@
-// not passing testcase
-
 string Solution::solve(string A) {
-    string x = "";
-    string y = "";
+    string x = "" , y ="";
+    if (A.size() == 1) return A;
     for ( int i = A.size()-1;i>=0;i--){
         if (A[i] == ' ' && A[i+1] == ' ') continue;
-        if (A[i] != ' '){
-            x = A[i] + x;
-        }
+        if (A[i] != ' ')  x = A[i] + x;
         else {
-            
                 y = y + " " + x;
-                x = "";
-            
+                x = ""; 
         }
-          if ( i == 0 && A[i] != ' ') {
-            y = y + " " + x;
-          }
+          if ( i == 0 && A[i] != ' ')  y = y + " " + x;
     }
-    y.erase(0,1);
+    int k =0;
+    while (k<y.size() && y[k]==' ') k++;
+    y.erase(0,k);
+    
     return y ;
 }
